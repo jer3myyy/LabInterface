43e85ba37f591cd5f20f2fb19bfbe0b2a623b9c4
jer3myyy <vabczlm@gmail.com>
2025-10-16 14:13:11 +0400

    Add files via upload
    attachments for laba

---
+++ app.js
@@ -0,0 +1,46 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вывести книги по фантастике");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+btn2.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вывести книги по детективу");
+        item = "2";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вывести книги по фэнтези");
+        item = "3";
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
